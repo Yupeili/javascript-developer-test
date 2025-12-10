9b03f1d11344de86b780e5d3e160f88b4c35b393
Yupei Li <yupeili@Yupeis-MacBook-Pro.local>
2025-12-10 11:12:37 +1100

    finishing test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,28 @@
 const { httpGet } = require('./mock-http-interface');
 
+/**
+ * Api caller.
+ * @param {string} str stringified json string.
+ * @returns {string} response message.
+ */
+const parseResponse = (str) => {
+  try {
+    return JSON.parse(str).message
+  }catch(e){
+    return ''
+  }
+}
+
+/**
+ * Api caller.
+ * @param {string[]} urls api urls.
+ * @returns {{status: number; body: string;}[]} an array of response.
+ */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = await Promise.all(urls.map(url=> httpGet(url)));
+  return results.map(res => res.status === 200 ? ({
+    'Arnie Quote': JSON.parse(res.body).message
+  }) : ({ 'FAILURE': JSON.parse(res.body).message }))
 };
 
 module.exports = {
